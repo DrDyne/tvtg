6afd1e54425ceadbd8658194354c7d2bb4d6d840
DrDyne <dyne@dyne-mint.(none)>
2012-09-16 11:15:42 -0400

    lots of works. did a good part of the UI, some more animations, single post view, mocked the data even more

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var sugar = require('sugar');
 var articleProvider = require('../mockArticles').ArticleProvider;
 
+function isAdmin () {
+  return true;
+};
 /*
  * GET home page.
  */
@@ -16,14 +19,14 @@ exports.post = function(req, res) {
   // should fetch data from DB instead of this ugly mock
   var article = {id:0, title : 'meh post !', caption: 'miffy le lapin', age: 0, author:'dyne', ago:function(){}};
   // should also set a real value to admin
-  res.render('index', { caption: '', admin : true, articles : [article] });
+  res.render('index', { caption: '', admin : isAdmin(), articles : [article] });
 };
 
 exports.insertPost = function(req, res) {
   ['caption', 'image', 'title', 'password'].each(function (prop) {
     console.log(req.param(prop) || prop + ' was empty');
   });
-  res.render('index', { caption: req.param('caption'), articles: [] });
+  res.render('index', { caption: req.param('caption'), admin : isAdmin(), articles: [] });
 };
 
 exports.deletePost = function(req, res) {
