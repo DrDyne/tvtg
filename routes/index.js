e19ee9ffd8668870991a381f30c3d7c174971e10
DrDyne <dyne@dyne-mint.(none)>
2012-09-16 06:15:52 -0400

    added a route to insert Posts in the DB

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
+var sugar = require('sugar');
 var articleProvider = require('../mockArticles').ArticleProvider;
-var fs = require('fs');
 
 /*
  * GET home page.
@@ -7,12 +7,20 @@ var fs = require('fs');
 
 exports.index = function(req, res){
   var articles = new ArticleProvider().findAll(function (err, articles) {
-    res.render('index', { title: 'Express', articles : articles });
+    res.render('index', { caption: '', articles : articles });
   });
 };
 
 exports.post = function(req, res ) {
   // should fetch data from DB instead of this ugly mock
   var article = {id:0, title : 'meh post !', caption: 'miffy le lapin', age: 0};
-  res.render('post', { article : article });
+  // should also set a real value to admin
+  res.render('post', { caption: '', admin : true, article : article });
+};
+
+exports.insertPost = function(req, res) {
+  ['caption', 'image', 'title', 'password'].each(function (prop) {
+    console.log(req.param(prop) || prop + ' was empty');
+  });
+  res.render('index', { caption: req.param('caption'), articles: [] });
 };
